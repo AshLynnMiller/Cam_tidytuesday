262b8f50e0d11a23a2a71b71136ae53f8f4c0f9b
camkay <ckay@uoregon.edu>
2022-07-20 11:45:21 -0400

    Exported and posted plot for Week 29.

--- tt_2022_29.R
+++ tt_2022_29.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(magrittr)
 library(extrafont)
+library(countrycode)
 
 # import fonts
 # font_import() # only run once
@@ -10,7 +11,7 @@ library(extrafont)
 fonts()
 
 # register fonts
-loadfonts(device = "all", quiet = TRUE)
+loadfonts(device = "all")
 
 # load data
 data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-19/technology.csv')
@@ -26,7 +27,7 @@ data %<>%
   mutate(proportion = value / value_sum) %>%
   mutate(label = gsub("Electricity from (.*) \\(TWH\\)", "\\1", label)) %>%
   mutate(label = Hmisc::capitalize(label)) %>%
-  mutate(iso3c = if_else(iso3c == "CAN", "Canada", iso3c)) %>%
+  mutate(iso3c = countrycode(iso3c, origin = "iso3c", destination = "country.name")) %>%
   mutate(label = factor(label, levels = c("Hydro",
                                           "Gas",
                                           "Coal",
@@ -37,10 +38,15 @@ data %<>%
                                           "Other renewables")))
 
 # plot
+png('~/Desktop/tt_2022_29.png', units = "in", width = 4, height = 2, res = 600)
+
 ggplot(data, aes(x = year, y = proportion, color = label)) +
-  # geom_smooth(se = FALSE, alpha = .80) +
-  geomtextpath::geom_textline(aes(label = label), stat = "smooth", 
-                linewidth = 1.5, hjust = .8, alpha = .60) +
+  geomtextpath::geom_textline(aes(label = label), 
+                              stat      = "smooth", 
+                              linewidth = .5, 
+                              hjust     = .8, 
+                              alpha     = .60, 
+                              size      = 2) +
   facet_wrap(~iso3c) +
   scale_color_manual(values = c("#4febff",
                                 "#ca6bfa",
@@ -50,29 +56,33 @@ ggplot(data, aes(x = year, y = proportion, color = label)) +
                                 "#e6f8fa",
                                 "#fff703",
                                 "#a1a1a1")) +
-  # scale_colour_viridis_d() +
-  scale_y_continuous(labels = scales::percent, limits = c(0, .70), breaks = seq(from = 0, to = .70, by = .10)) +
-  labs(title  = "Energy Production",
-      caption = "Tidy Tuesday 2022 - Week 29 | Data Source: CHAT | Visualization: @cameronskay",
-      x       = "Year", 
-      y       = "Percent of Total Production",
-      color   = "") +
-   # geom_label(aes(label = label),
-   #            data = filter(data, year == 2000),
-   #            size = 4) +
+  scale_y_continuous(labels = scales::percent, 
+                     limits = c(0, .70), 
+                     breaks = seq(from = 0, to = .70, by = .10)) +
+  labs(title   = "Energy Production",
+      subtitle = "Tidy Tuesday 2022 - Week 29 | Data Source: CHAT | Visualization: @cameronskay",
+      x        = "Year", 
+      y        = "Percent of total production",
+      color    = "") +
   theme_minimal() %+replace% 
     theme(
-      
-      legend.position = "none",
-      text = element_text(size = 15, color = "white"),
-      legend.text = element_text(color = "white"),
-      strip.text = element_text(color = "white"),
-      plot.title      = element_text(hjust = 0.5, size = 20),
-      plot.background = element_rect(fill = "black"),
+      text             = element_text(family = "Avenir Next Regular", size = 6, color = "white"),
+      legend.position  = "none",
+      legend.text      = element_text(color = "white"),
+      strip.text       = element_text(size = 6, color = "white"),
+      plot.title       = element_text(size = 7, family = "Avenir Next Bold", vjust = 6, hjust = 0.5),
+      plot.subtitle    = element_text(size = 4, family = "Avenir Next Regular", vjust = 7, hjust = 0.5),
+      plot.caption     = element_text(size = 4, vjust = -11, hjust = 1.08),
+      plot.background  = element_rect(fill = "black"),
+      panel.grid       = element_blank(),
       panel.background = element_rect(fill = "black", color = "black"),
-      panel.grid      = element_blank(),
-      axis.title      = element_text(size = 15,color = "white"),
-      axis.text       =  element_text(size = 10, color = "white"),
-      axis.line       =  element_line(color = "white"),
-      plot.caption    =  element_text())
+      axis.title       = element_text(size = 6,color = "white"),
+      axis.text        = element_text(size = 5, color = "white"),
+      axis.text.y      = element_text(size = 5, color = "white"),
+      axis.title.x     = element_text(vjust = -3),
+      axis.title.y     = element_text(angle = 90, hjust = .5, vjust = 5),
+      axis.line        = element_line(color = "white"),
+      plot.margin      = margin(.6, .4, .5, .6, "cm"))
+
+dev.off()
 
